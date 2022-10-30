330e2cac7b982f26ab78d60df465f8d81be3388a
princerawal <prr45@scarletmail.rutgers.edu>
2022-10-30 14:05:12 -0400

    Added match

--- src/Account.js
+++ src/Account.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import SideBar from './components/Sidebar';
+import { Form, Button } from "react-bootstrap";
 import "./Account.css";
 import axios from "axios";
+import Match from "./Match";
 const Account = (props) => {
 
   const [firstName, setFirstName] = useState(null);
@@ -24,13 +26,31 @@ const Account = (props) => {
       console.log(error)
       error = new Error();
     });
+
+    const handleMatch= (event) => {
+      const element = <Match userName = {props.userName} firstName = {firstName} lastName = {lastName} root = {props.root}/>;
+      props.root.render(element);
+    };
+
   return (
     <>
     <div id="Account">
       <SideBar />
       <div id="page-wrap">
         <h1>Account</h1>
-  <h2>Welcome {firstName} {lastName}!</h2>
+        <h2>Welcome {firstName} {lastName}!</h2>
+
+        <div>Click here to see students who are a great match for you -> 
+
+        <Button
+          variant="primary"
+          type="submit"
+          onClick={(e) => handleMatch(e)}
+        >
+          Go match!
+        </Button>
+        </div>
+    
       </div>
     </div>
 
--- src/Match.js
+++ src/Match.js
@@ -4,12 +4,13 @@ import "./Account.css";
 import axios from "axios";
 const Match = (props) => {
 
-  const [firstName, setFirstName] = useState(null);
-  const [lastName, setLastName] = useState(null);
+  const [firstName, setFirstName] = useState(props.firstName);
+  const [lastName, setLastName] = useState(props.lastName);
+  const [ids, setIds] = useState([]);
 
   const configuration = {
     method: "get",
-    url: "http://localhost:8080/api/students/getStudent/" + props.userName,
+    url: "http://localhost:8080/api/students/matches/" + props.userName,
   };
   console.log(configuration);
 
@@ -17,20 +18,42 @@ const Match = (props) => {
   
   axios(configuration)
     .then((result) => {
-        setFirstName(result.data.firstName);
-        setLastName(result.data.lastName);
+        setIds(result.data);
       })
     .catch((error) => {
       console.log(error)
       error = new Error();
     });
+
+
   return (
     <>
     <div id="Account">
       <SideBar />
       <div id="page-wrap">
         <h1>Account</h1>
-  <h2>Welcome {firstName} {lastName}!</h2>
+        <h2>Welcome {firstName} {lastName}!</h2>
+        
+        <div className="Match">
+          <table>
+            <tr>
+              <th>Name</th>
+              <th>Year</th>
+              <th>Pronouns</th>
+            </tr>
+            {ids.map((val, key) => {
+              return (
+                <tr key={key}>
+                  <td>{val.userName}</td>
+                  <td>{val.year}</td>
+                  <td>{val.pronouns}</td>
+                </tr>
+              )
+            })}
+          </table>
+        </div>
+
+
       </div>
     </div>
 
